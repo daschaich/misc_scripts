8d9f65b95cff9469ae2c2dc57b99e1cc6401f134
daschaich <daschaich@gmail.com>
2019-02-12 17:04:45 +0100

    Checkpoint experimentation with fixed height in Polyakov loop histogram plots

--- poly_hist.py
+++ poly_hist.py
@@ -11,9 +11,10 @@ import matplotlib.pyplot as plt
 # Extract $tag from path rather than input argument
 
 # Parse arguments: First whether we're considering poly or Wpoly
-# Then thermalization cuta and how many bins to have in the histogram
+# Then thermalization cut, horizontal and vertical maxima,
+# and how many bins to have in the histogram
 if len(sys.argv) < 3:
-  print "Usage:", str(sys.argv[0]), "<file> <cut> <bins>"
+  print "Usage:", str(sys.argv[0]), "<file> <cut> <xmax> <ymax> <bins>"
   sys.exit(1)
 
 # This should be either poly_mod or Wpoly_mod
@@ -21,15 +22,20 @@ poly_file = str(sys.argv[1])
 
 cut = int(sys.argv[2])
 xmax = float(sys.argv[3])
-nbins = int(sys.argv[4])
+ymax = int(sys.argv[4])
+nbins = int(sys.argv[5])
 
 # First make sure we're calling this from the right place
+# Then extract tag from path as everything after the last '/'
 if not os.path.isdir('data'):
   print "ERROR: data/ does not exist"
   sys.exit(1)
-
-# Extract tag from path as everything after the last '/'
 cwd = os.getcwd()
+if 'SU4' in cwd:
+  Nc = 4.0
+else:
+  print "ERROR: Only expecting SU4 at the moment"
+  sys.exit(1)
 tag = (cwd.split('/'))[-1]
 
 # Prepare data to plot, using c=0.5 if Wflowed
@@ -38,7 +44,7 @@ tag = (cwd.split('/'))[-1]
 # Wpoly format: MDTU,c=0.2,0.3,0.4,0.5    (normalized to Nc)
 dat = []
 toOpen = 'data/' + poly_file + '.csv'
-for line in open():
+for line in open(toOpen):
   if line.startswith('M'):
     continue
   temp = line.split(',')
@@ -49,27 +55,22 @@ for line in open():
     dat.append(float(temp[4]))
   else:
     dat.append(Nc * float(temp[1]))
-  dat.append(float(temp[4]))
 
 # Print number of measurements to allow offline checks
 print "%d measurements for %s" % (len(dat), tag)
 
 # Create histogram
 plt.figure(figsize=(6.40, 3.84))    # Gnuplot default
-plt.hist(dat, bins=np.arange(0.0, xmax, xmax / nbins),
-         log=False, density=False, align='mid',
+plt.hist(dat, nbins, log=False, normed=True, align='mid',
          edgecolor='blue', label=tag, histtype='step', hatch='//')
 
-plt.xlim([0.0, xmax])
+plt.axis([0.0, xmax, 0.0, ymax])
 plt.grid(False)
-
 if poly_file.startswith('W'):
-  plt.title('Wflowed Polyakov loop magnitude, ' + title)
   plt.xlabel('|PL_W|')
 else:
-  plt.title('Polyakov loop magnitude, ' + title)
   plt.xlabel('|PL|')
-plt.ylabel('Count')
+plt.ylabel('Relative frequency')
 plt.legend()
 
 # Save a pdf
