caec4809fa9d695fed994a01ddc26850c55b6130
daschaich <daschaich@gmail.com>
2019-06-16 11:36:19 +0100

    Helper file for organizing SU(4) histogram plots online

---
+++ prep_hist_index.sh
@@ -0,0 +1,16 @@
+echo "  <p>Last update: `date`</p>"
+echo "  <ul>"
+echo "    <li><a href="poly_hist_m1.gif">poly_hist_m1.gif</a></li>"
+echo "    <li><a href="Wpoly_hist_m1.gif">Wpoly_hist_m1.gif</a></li>"
+echo "    <br>"
+echo "    <li><a href="poly_hist_m2.gif">poly_hist_m2.gif</a></li>"
+echo "    <li><a href="Wpoly_hist_m2.gif">Wpoly_hist_m2.gif</a></li>"
+for m in 1 2 ; do
+  for i in poly_hist_b*_m0.$m.pdf ; do
+    echo "    <br>"
+    echo "    <li><a href="$i">$i</a></li>"
+    echo "    <li><a href="W$i">W$i</a></li>"
+  done
+done
+echo "  </ul>"
+echo "</body></html>"
